ea1756cbcbc9ebff412d32ed06293009fda85fd6
Banzay <yuriy.zdyrko@gmail.com>
2018-03-29 22:48:50 +0300

    udpate test
    review

--- server/tests/routes/product.spec.js
+++ server/tests/routes/product.spec.js
@@ -25,9 +25,7 @@ describe('Tests for product api', () => {
         });
     });
 
-    it('Checking created items', function () {
-
-        return new Promise((resolve, reject) => {
+    it('Checking created items', async function () {
           let insertedProducts = [
             {
                 title: 'titleTest1',
@@ -50,22 +48,12 @@ describe('Tests for product api', () => {
                 description: 'descriptionTest5'
             }
           ]
-          Product.collection.insert(insertedProducts, (err, products) => {
-            if(err) {
-              reject(new Error("Error during saving products: " + JSON.stringify(err)));
-            } else {
-              Product.find({}, (err, products) => {
-                console.log(products, "PRODUCTS   ");
-                if(err) {
-                  reject(new Error("Error during getting products: " + JSON.stringify(err)));
-                } else {
-                  expect(products).to.exist;
-                  expect(products.length).to.be.equal(5);
-                  resolve();
-                }
-              });
-            }
-          });
+          
+          
+          const products = await Product.collection.insert(insertedProducts)
+          const saved = await Product.find({})
+          expect(saved).to.exist;
+          expect(saved.length).to.be.equal(5);
         });
     });
 });
